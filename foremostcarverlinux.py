e5fa0c467ab2ec2cac556059d37867313ce68d4b
vaexa <kobuspoes@gmail.com>
2018-12-20 16:33:53 +0100

    finalversie 20-12

--- foremostcarverlinux.py
+++ foremostcarverlinux.py
@@ -12,14 +12,14 @@ def foremostapt():
     except KeyError:
         print("Foremost is niet gevonden op dit apparaat.")
         print("Foremost is benodigd om te carven en wordt nu geïnstalleerd.")
-        subprocess.call("sudo apt-get update")
-        subprocess.call("sudo apt-get install foremost")
+        subprocess.call(["sudo", "apt-get", "update"])
+        subprocess.call(["sudo", "apt-get", "install", "foremost"])
 
 def carve(evidenceid):
     foremostapt()
     try:
         cfile = toolinputs.carve_input()
-        crv = subprocess.call(["foremost","-t all",cfile,"-o","output" + evidenceid])
+        crv = subprocess.call(["foremost","-t","all","-o","output" + evidenceid, "-i",cfile])
         return crv
     except():
         print("Carven mislukt")
