c75131c5cdef27b1ce7d39b57236359647b114f9
vaexa <kobuspoes@gmail.com>
2018-12-20 16:13:48 +0100

    typo fix

--- ddimage.py
+++ ddimage.py
@@ -7,7 +7,7 @@ import shlex
 def imager(evidenceid):
     ifile = toolinputs.image_input_station()
     #bsize = toolinputs.image_input_blocksize()
-    cmd = subprocess.call(['dd', 'if=' + ifile, 'of=/home/maarten/Desktop/' + "image" + evidenceid + '.raw'],stdout=PIPE)
+    cmd = subprocess.call(['dd', 'if=' + ifile, 'of=/home/maarten/Desktop/' + "image" + evidenceid + '.raw'])
     progress = cmd.stdout.readline()
     print(progress)
     defaultlogger.logging.info("het pad van de geimagde usb stick is " + ifile + ", locatie en naam van image is" + ("image" + evidenceid) + "met blocksize van" + bsize)
