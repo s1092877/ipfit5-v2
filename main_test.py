1199f709025fc9bbd7d147371814b84fb0456f58
s1092877 <36610019+s1092877@users.noreply.github.com>
2018-12-20 13:18:40 +0100

    Delete main_test.py

--- main_test.py
+++
@@ -1,7 +0,0 @@
-#tijdelijk testbestand om snel en zonder complicaties diverse modules en features te testen
-#lever dit script niet op
-
-import defaultlogger
-import toolinputs
-
-toolinputs.os_choice_input()
