2a2ebcd6c3a94839059dbd9e4c07a653f61c8d6a
vaexa <kobuspoes@gmail.com>
2019-01-10 13:18:49 +0100

    output path fix

--- ddimage.py
+++ ddimage.py
@@ -1,15 +1,15 @@
 import subprocess
 import defaultlogger
 import toolinputs
-import shlex
 
 
 def imager(evidenceid):
     ifile = toolinputs.image_input_station()
     #bsize = toolinputs.image_input_blocksize()
-    cmd = subprocess.call(['dd', 'if=' + ifile, 'of=/home/maarten/Desktop/' + "image" + evidenceid + '.raw'])
+    cmd = subprocess.call(['dd', 'if=' + ifile, 'of=images/' + "image" + evidenceid + '.raw'])
     progress = cmd.stdout.readline()
     print(progress)
-    defaultlogger.logging.info("het pad van de geimagde usb stick is " + ifile + ", locatie en naam van image is" + ("image" + evidenceid) + "met blocksize van" + bsize)
+    defaultlogger.logging.info("het pad van de geimagede usb stick is " + ifile + ", locatie en naam van image is" + ("image" + evidenceid) + "met blocksize van" + bsize)
+    return ifile
 
 
