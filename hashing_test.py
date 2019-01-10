3cc5b539c298e4381b6b35c210c20d616dfe4925
vaexa <kobuspoes@gmail.com>
2019-01-10 14:50:40 +0100

    demo 11 jan 2

--- hashing_test.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-BLOCKSIZE = 65536
-hasher = hashlib.md5() #we maken een md5 hash
-with open('2018-11-13-raspbian-stretch-full.img', 'rb') as afile: #Hierbij open je het het bestand dat je wil hashen, dat is nu raspian strech
-    buf = afile.read(BLOCKSIZE)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(BLOCKSIZE)
-print(hasher.hexdigest()) # hierbij print je de hash uit die je net hebt gemaakt
